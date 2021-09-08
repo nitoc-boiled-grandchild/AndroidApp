dddde0cee7b4e7da170a64c1e3aa68e949daaa87
hart1222 <c.haruto.2727@gmail.com>
2021-09-08 22:12:22 +0900

    apuri2

--- app/src/main/java/com/github/kazu_yamy/remindcell/MainActivity.kt
+++ app/src/main/java/com/github/kazu_yamy/remindcell/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.kazu_yamy.remindcell
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Build
@@ -7,17 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_week.*
 import kotlinx.android.synthetic.main.activity_weekset.*
+import kotlinx.android.synthetic.main.activity_dateset.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 import android.content.Intent
+import kotlinx.android.synthetic.main.activity_tag.*
+import kotlinx.android.synthetic.main.activity_weekset.button2
 
 
 class MainActivity : AppCompatActivity() {
-
-
+    companion object {
+        val day1 = arrayOf("月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日")
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,20 +29,26 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        Date()
+        showDate()
+
+        button3.setOnClickListener {
+            registration()
+        }
 
         Button.setOnClickListener {
             showDatePicker()
         }
 
         Button1.setOnClickListener {
-            showWeekPicker()
+                showWeekPicker1()
         }
+
+
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun Date(){
-        val calendar: Calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN);
+    private fun showDate(){
+        val calendar: Calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
 
         val current = LocalDate.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日")
@@ -46,116 +56,79 @@ class MainActivity : AppCompatActivity() {
 
         textView5.text= formatted
 
-        val day = calendar.get(Calendar.DAY_OF_WEEK)
-        if (day == 1) {
-            textView8.text = "日曜日"
-            textView8.setTextColor(Color.RED)
-        }else if(day == 2) {
-            textView8.text = "月曜日"
-            textView8.setTextColor(Color.BLACK)
-        }else if(day == 3) {
-            textView8.text = "火曜日"
-            textView8.setTextColor(Color.MAGENTA)
-        }else if(day == 4) {
-            textView8.text = "水曜日"
-            textView8.setTextColor(Color.CYAN)
-        }else if(day == 5) {
-            textView8.text = "木曜日"
-            textView8.setTextColor(Color.GREEN)
-        }else if(day == 6) {
-            textView8.text = "金曜日"
-            textView8.setTextColor(Color.YELLOW)
-        }else {
-            textView8.text = "土曜日"
-            textView8.setTextColor(Color.BLUE)
+        when (calendar.get(Calendar.DAY_OF_WEEK)) {
+            1 -> {
+                textView8.text = "日曜日"
+                textView8.setTextColor(Color.RED)
+            }
+            2 -> {
+                textView8.text = "月曜日"
+                textView8.setTextColor(Color.BLACK)
+            }
+            3 -> {
+                textView8.text = "火曜日"
+                textView8.setTextColor(Color.MAGENTA)
+            }
+            4 -> {
+                textView8.text = "水曜日"
+                textView8.setTextColor(Color.CYAN)
+            }
+            5 -> {
+                textView8.text = "木曜日"
+                textView8.setTextColor(Color.GREEN)
+            }
+            6 -> {
+                textView8.text = "金曜日"
+                textView8.setTextColor(Color.YELLOW)
+            }
+            else -> {
+                textView8.text = "土曜日"
+                textView8.setTextColor(Color.BLUE)
+            }
         }
     }
 
     private fun showDatePicker() {
-
+        val calendar: Calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
+        val year1 = calendar.get(Calendar.YEAR)
+        val month1 = calendar.get(Calendar.MONTH)
+        val date1 = calendar.get(Calendar.DATE)
 
         val datePickerDialog = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener() { view, year, month, dayOfMonth ->
-                textView.text = "選択した日付は「${year}/${month + 1}/${dayOfMonth}」です"
+            { _, year, month, dayOfMonth ->
+                setContentView(R.layout.activity_dateset)
+                textView9.text = "${year}年${month+1}月${dayOfMonth}日の持ち物"
             },
-            2021,
-            7,
-            25//ここ今日の日付でやりたいんだけどできないから数字で代用してる
+            year1,
+            month1,
+            date1
         )
         datePickerDialog.show()
     }
 
-    private fun showWeekPicker() {
-        setContentView(R.layout.activity_week)
+    private fun showWeekPicker1() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("曜日の選択")
+            .setItems(day1) { _, which ->
+                setContentView(R.layout.activity_weekset)
+                textView4.text = "${day1[which]}の持ち物"
+                button2.setOnClickListener {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            .setNegativeButton("閉じる",null)
+            .create()
+        dialog.show()
+    }
 
-        Button2.setOnClickListener {
+    private fun registration() {
+        setContentView(R.layout.activity_tag)
+
+        button5.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }
-
-        Button11.setOnClickListener {
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "月曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button12.setOnClickListener {
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "火曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button13.setOnClickListener{
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "水曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button21.setOnClickListener{
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "木曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button22.setOnClickListener{
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "金曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button31.setOnClickListener{
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "土曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
-        }
-        Button32.setOnClickListener{
-            setContentView(R.layout.activity_weekset)
-
-            textView4.text = "日曜日の持ち物"
-
-            button2.setOnClickListener {
-                showWeekPicker()
-            }
         }
     }
 }
